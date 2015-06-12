31b0a3f5347c802359120d8ad3e124795de31460
dbushkanets <dbushkanets@harryfox.com>
2015-06-12 01:21:45 -0400

    Fixed indentation

--- app/API.php
+++ app/API.php
@@ -6,30 +6,30 @@ use Illuminate\Database\Eloquent\Model;
 
 class API extends Model
 {
-	/**
-	 * Initiate get request from github api
-	 * @param  string  $uri  api uri to dictate 
-	 * which api to hit with what parameters
-	 * 
-	 * @param  boolean $json flag to denote whether 
-	 * to return object in json or raw format
-	 * 
-	 * @return json | string api result set
-	 */
+    /**
+     * Initiate get request from github api
+     * @param  string  $uri  api uri to dictate 
+     * which api to hit with what parameters
+     * 
+     * @param  boolean $json flag to denote whether 
+     * to return object in json or raw format
+     * 
+     * @return json | string api result set
+     */
     static function get($uri, $array = true)
     {
-		$ch = curl_init();
+        $ch = curl_init();
 
-	    $options = array(
-	    	CURLOPT_URL => getenv("GITHUB_API").$uri, 
-	    	CURLOPT_RETURNTRANSFER => 1, 
-	    	CURLOPT_HTTPHEADER => array(
-	    		"User-Agent:".getenv("APP_NAME")));
+        $options = array(
+            CURLOPT_URL => getenv("GITHUB_API").$uri, 
+            CURLOPT_RETURNTRANSFER => 1, 
+            CURLOPT_HTTPHEADER => array(
+                "User-Agent:".getenv("APP_NAME")));
 
-	    curl_setopt_array($ch, $options);
+        curl_setopt_array($ch, $options);
 
-	    $data = curl_exec($ch);
+        $data = curl_exec($ch);
 
-	    return $array ? json_decode($data, true) : $data;
+        return $array ? json_decode($data, true) : $data;
     }
 }
