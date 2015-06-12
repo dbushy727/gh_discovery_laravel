f48de82a01d032bcd6d539a84eed2c6ced38a169
dbushkanets <dbushkanets@harryfox.com>
2015-06-11 21:34:28 -0400

    create get request model method for github api

--- app/API.php
+++ app/API.php
@@ -6,5 +6,30 @@ use Illuminate\Database\Eloquent\Model;
 
 class API extends Model
 {
-    //
+	/**
+	 * Initiate get request from github api
+	 * @param  string  $uri  api uri to dictate 
+	 * which api to hit with what parameters
+	 * 
+	 * @param  boolean $json flag to denote whether 
+	 * to return object in json or raw format
+	 * 
+	 * @return json | string api result set
+	 */
+    function get($uri, $json = true)
+    {
+		$ch = curl_init();
+
+	    $options = array(
+	    	CURLOPT_URL => getenv(GITHUB_API).$uri, 
+	    	CURLOPT_RETURNTRANSFER => 1, 
+	    	CURLOPT_HTTPHEADER => array(
+	    		"User-Agent:".getenv(APP_NAME)));
+
+	    curl_setopt_array($ch, $options);
+
+	    $data = curl_exec($ch);
+
+	    return $json ? json_decode($data, true) : $data;
+    }
 }
