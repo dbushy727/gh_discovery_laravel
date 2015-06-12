5eaf427f807544b98467e91a443085c5f9a93f01
dbushkanets <dbushkanets@harryfox.com>
2015-06-11 21:38:50 -0400

    create ability to search the github api and retrieve a json reponse

--- app/Repo.php
+++ app/Repo.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Repo extends Model
 {
-    //
+	/**
+	 * search the github api for a query and/or language
+	 * @param  string  $query      	search string
+	 * @param  string  $language   	choose language to filter
+	 * @param  integer $page_limit 	set result limit
+	 * 
+	 * @return json
+	 */
+    function search($query, $language = '', $page_limit = 25)
+    {
+    	if(!empty($language))
+    	{
+    		$query .= "+language:$language";
+    	}
+
+    	$uri = "search/repositories?q={$query}&per_page={$page_limit}";
+    	return API::get($uri);
+    }
 }
